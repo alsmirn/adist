aa1431010bf287612ee2cdb5d249917ff75af066
Alexey Smirnov <alsmirn@gmail.com>
2009-12-04 20:07:02 +0300

    license note was added to main

--- adist.py
+++ adist.py
@@ -1,12 +1,37 @@
 #!/usr/bin/env python
+"""
+Copyright (c) 2009, Alexey Smirnov
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the Saint-Petersburg State University nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY ALEXEY SMIRNOV ''AS IS'' AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL ALEXEY SMIRNOV BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
 
 from extinction import arenou, tabular
-import extinction
 
 def photo_dist(v_band, b_v, t_class, s_class, l_class, **kwargs):
     """
     Positional arguments:
-    @param v_band: visual magnitude
+    @param v_band: visual magnitude. Float.
     @param b_v: B-V value. Float. 
     @param t_class: Temperature class, from list 'OBAFGKM'. String.
     @param s_class: Temperature subclass, from 0 to 9. Integer.
