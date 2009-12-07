9ceff93c32ee06ffed7378681e6480799868d466
Alexey Smirnov <alsmirn@gmail.com>
2009-12-07 20:50:29 +0300

    argumets check in photo_dist was improved

--- adist.py
+++ adist.py
@@ -46,42 +46,47 @@ def photo_dist(v_band, b_v, t_class, s_class, l_class, **kwargs):
     
     @author: Alexey Smirnov
     """
-    #@TODO: params check!
-    __params = ('extinction', 'dist', 'l', 'b', )
     
     # full extinction
     a_v = float(0)
     
-    # interstellar extinction mode
-    extinction = kwargs.pop('extinction')
+    # interstellar extinction mode detection
+    try:
+        extinction = kwargs.pop('extinction')
+    except KeyError:
+        extinction = str("No extinction models used.")
     
+    # extinction model choice
     if extinction == 'arenou':
-        dist = kwargs.pop('dist')
-        l = kwargs.pop('l')
-        b = kwargs.pop('b')
-        
+        try: 
+            dist = kwargs.pop('dist')
+            l = kwargs.pop('l')
+            b = kwargs.pop('b')
+        except KeyError:
+            print "Not enough arguments to applying %s method." % extinction
+            import sys
+            sys.exit(True)
         a_v = arenou.av_arenou(dist, l, b)
-        
     elif extinction == 'tabular':
         a_v = tabular.av_tabular(t_class, s_class, l_class, b_v)
-        
     else:
         a_v = float(0)
         
     do_key = lambda *args: "%s%d%d" % args
     TSL = do_key(t_class, s_class, l_class)
     
-    photo_dist = 10.0**(0.2*(v_band - tabular.STD_V[TSL] - a_v + 5.0))
-        
+    photo_dist = 10.0**(0.2 * (v_band - tabular.STD_V[TSL] - a_v + 5.0))
+    
     return photo_dist  
 
 def main():
     from time import time
     from random import randint
     beg = time()
-    for i in xrange(1000000):
+    for i in xrange(100000):
         photo_dist(10, 4, 'O', 7, 5, extinction='arenou', 
                    dist=randint(0,1000), l=randint(0,361), b=randint(-90,91))
+        #photo_dist(10, 4, 'O', 7, 5, extinction='tabular', )
     print time() - beg
     
 main()
